ddb3815aad595b220f2652020364e5d7156a995a
Mukhammadamin Abdullaev <mukhammadaminabdullaev01082002>
2021-03-10 13:09:46 +0500

    Main Pages Made Linked

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',HomeComponent::class);
+
+Route::get('/shop',ShopComponent::class);
+
+Route::get('/cart',CartComponent::class);
+
+Route::get('/checkout',CheckoutComponent::class);
