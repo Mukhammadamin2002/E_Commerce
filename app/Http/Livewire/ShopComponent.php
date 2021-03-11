1068f79a327fa7bcdfe2905c290a086a70fa5f4f
Mukhammadamin Abdullaev <mukhammadaminabdullaev01082002>
2021-03-11 13:49:43 +0500

    Shop Page Datas Have Made Dynamic

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -3,11 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Product;
 
 class ShopComponent extends Component
 {
+	use WithPagination;
     public function render()
     {
-        return view('livewire.shop-component')->layout("layouts.base");
+    	$products = Product::paginate(12);
+        return view('livewire.shop-component',['products' => $products])->layout("layouts.base");
     }
 }
