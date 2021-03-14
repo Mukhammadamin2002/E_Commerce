2cc0e66c6b0983e5a9c81945267767aac5c4b8e1
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-14 18:12:16 +0500

    Changes Made

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -22,7 +22,7 @@
          </div>
     				</div>
     				<div class="panel-body">
-    					<table class="table table-bordered table-hover">
+    					<table class="table table-bordered table-stripped">
     						<thead>
     							<tr>
     								<th>Id</th>
