dae26a9cb0b27efc8b3b644bb478612aa839ea28
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-18 00:14:26 +0500

    Table Made For Slider Data On Admin Page

--- resources/views/livewire/admin/admin-add-home-slider-component.blade.php
+++ resources/views/livewire/admin/admin-add-home-slider-component.blade.php
@@ -1,3 +1,51 @@
 <div>
-    {{-- Be like water. --}}
+    <div class="container" style="padding: 30px 0;">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<div class="panel panel-default">
+    				<div class="panel-heading">
+    					<div class="row">
+    						<div class="col-md-6">
+    							<b>Add New Slide</b>
+    						</div>
+    						<div class="col-md-6">
+    							<a href="{{route('admin.homeslider')}}" class="btn btn-success pull-right">All Slides</a>
+    						</div>
+    					</div>
+    				</div>
+    				<div class="panel-body">
+    					<form action="" class="form-horizontal">
+    						<div class="form-group">
+    							<label for="" class="col-md-4 cotrol-label">Title</label>
+    							<div class="col-md-4">
+    								<input type="text" placeholder="Title" class="for=m-control input-md ">
+    							</div>
+    						</div>
+
+    						<div class="form-group">
+    							<label for="" class="col-md-4 cotrol-label">Subtitle</label>
+    							<div class="col-md-4">
+    								<input type="text" placeholder="Subtitle" class="for=m-control input-md ">
+    							</div>
+    						</div>
+
+    						<div class="form-group">
+    							<label for="" class="col-md-4 cotrol-label">Title</label>
+    							<div class="col-md-4">
+    								<input type="text" placeholder="Title" class="for=m-control input-md ">
+    							</div>
+    						</div>
+
+    						<div class="form-group">
+    							<label for="" class="col-md-4 cotrol-label">Title</label>
+    							<div class="col-md-4">
+    								<input type="text" placeholder="Title" class="for=m-control input-md ">
+    							</div>
+    						</div>
+    					</form>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
 </div>
--- resources/views/livewire/admin/admin-home-slider-component.blade.php
+++ resources/views/livewire/admin/admin-home-slider-component.blade.php
@@ -1,3 +1,52 @@
 <div>
-    {{-- The whole world belongs to you --}}
+    <div class="container" style="padding: 30px 0;">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<div class="panel panel-default">
+    				<div class="panel-heading">
+    				    <div class="row">
+    				    	<div class="col-md-6">
+    				    		<b>All Slides</b>
+    				    	</div>
+    				    	<div class="col-md-6">
+    				    		<a href="{{route('admin.addhomeslider')}}" class="btn btn-success pull-right">Add New Slide</a>
+    				    	</div>
+    				    </div>
+    				</div>
+    				<div class="panel-body">
+    					<table class="table table-striped table-bordered">
+    						<thead>
+    							<tr>
+    								<th>Id</th>
+    								<th>Image</th>
+    								<th>Title</th>
+    								<th>Subtitle</th>
+    								<th>Price</th>
+    								<th>Link</th>
+    								<th>Status</th>
+    								<th>Date</th>
+    								<th>Action</th>
+    							</tr>
+    						</thead>
+    						<tbody>
+    							@foreach ($sliders as $slider)
+                                    <tr>
+                                    	<td>{{$slider->id}}</td>
+                                    	<td><img src="{{asset('assets/images/sliders')}}/{{$slider->image}}" width="120" alt=""></td>
+                                    	<td>{{$slider->title}}</td>
+                                    	<td>{{$slider->subtitle}}</td>
+                                    	<td>{{$slider->price}}</td>
+                                    	<td>{{$slider->link}}</td>
+                                    	<td>{{$slider->status == 1 ? 'Active':'Inactive'}}</td>
+                                    	<td>{{$slider->created_at}}</td>
+                                    	<td></td>
+                                    </tr>
+    							@endforeach
+    						</tbody>
+    					</table>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
 </div>
