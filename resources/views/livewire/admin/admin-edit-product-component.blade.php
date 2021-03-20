642ca5b25aa5e6deacefdff34440d5f09f088d41
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-20 23:37:03 +0500

    Html Editor Added On Admin Product Update Page

--- resources/views/livewire/admin/admin-edit-product-component.blade.php
+++ resources/views/livewire/admin/admin-edit-product-component.blade.php
@@ -44,18 +44,18 @@
     							<label for="" class="col-md-4 control-label">
     								Short Description
     							</label>
-    							<div class="col-md-4">
-    								<textarea class="form-control" placeholder="Short Description" wire:model="short_description"></textarea>
+    							<div class="col-md-4" wire:ignore>
+    								<textarea class="form-control" id="short_description" placeholder="Short Description" wire:model="short_description"></textarea>
                                     @error('short_description') <p class="text-danger">{{$message}}</p> @enderror
     							</div>
     						</div>
 
-    						<div class="form-group">
+    						<div class="form-group" wire:ignore>
     							<label for="" class="col-md-4 control-label">
     								Description
     							</label>
     							<div class="col-md-4">
-    								<textarea class="form-control" placeholder="Description" wire:model="description"></textarea>
+    								<textarea class="form-control" id="description" placeholder="Description" wire:model="description"></textarea>
                                     @error('description') <p class="text-danger">{{$message}}</p> @enderror
     							</div>
     						</div>
@@ -169,3 +169,30 @@
     	</div>
     </div>
 </div>
+
+@push('scripts')
+    <script>
+        $(function(){
+            tinymce.init({
+                selector:'#short_description',
+                setup:function(editor){
+                    editor.on('Change',function(e){
+                        tinyMCE.triggerSave();
+                        var sd_data = $('#short_description').val();
+                        @this.set('short_description',sd_data);
+                    });
+                }
+            });
+            tinymce.init({
+                selector:'#description',
+                setup:function(editor){
+                    editor.on('Change',function(e){
+                        tinyMCE.triggerSave();
+                        var d_data = $('#description').val();
+                        @this.set('description',d_data);
+                    });
+                }
+            });        
+        });
+    </script>
+@endpush
