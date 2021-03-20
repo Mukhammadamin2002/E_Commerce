ab6c805b2f1d8890567355b3cf0c40fe71c33f96
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-20 10:10:28 +0500

    Price Filter Applied On Product

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -37,19 +37,19 @@ class ShopComponent extends Component
     {
     	if($this->sorting == 'date')
     	{
-	    	$products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+	    	$products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('created_at','DESC')->paginate($this->pagesize);
     	}
     	else if ($this->sorting == 'price')
     	{
-	    	$products = Product::orderBy('regular_price','ASC')->paginate($this->pagesize);
+	    	$products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('regular_price','ASC')->paginate($this->pagesize);
     	}
 
     	else if ($this->sorting == 'price-desc')
     	{
-	    	$products = Product::orderBy('regular_price','DESC')->paginate($this->pagesize);
+	    	$products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('regular_price','DESC')->paginate($this->pagesize);
     	}
     	else {
-	    	$products = Product::paginate($this->pagesize);
+	    	$products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->paginate($this->pagesize);
     	}
 
     	$categories = Category::all();
