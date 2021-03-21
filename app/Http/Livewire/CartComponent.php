36c57da6ee725a7fafcf982e838b65667c2cb927
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-21 12:50:06 +0500

    Add Product To Wishlist Activated

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -12,6 +12,7 @@ class CartComponent extends Component
 		$product = Cart::instance('cart')->get($rowId);
 		$qty = $product->qty + 1;
 		Cart::instance('cart')->update($rowId,$qty);
+		$this->emitTo('cart-count-component','refreshComponent');
 	}
 
 	public function decreaseQuantity($rowId)
@@ -19,17 +20,20 @@ class CartComponent extends Component
 		$product = Cart::instance('cart')->get($rowId);
 		$qty = $product->qty - 1;
 		Cart::instance('cart')->update($rowId,$qty);
+		$this->emitTo('cart-count-component','refreshComponent');
 	}
 
 	public function delete($rowId)
 	{
 		Cart::instance('cart')->remove($rowId);
+		$this->emitTo('cart-count-component','refreshComponent');
 		session()->flash('success_message','Item has been removed');
 	}
 
 	public function destroyAll()
 	{
 		Cart::instance('cart')->destroy();
+		$this->emitTo('cart-count-component','refreshComponent');
 	}
     public function render()
     {
