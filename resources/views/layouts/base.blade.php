ff38d4cc6ac36d01594e3fab1143beed03254ea1
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-21 11:32:51 +0500

    Add Product To Wishlist Activated

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -140,9 +140,11 @@
 						<div class="wrap-icon right-section">
 							<div class="wrap-icon-section wishlist">
 								<a href="#" class="link-direction">
-									<i class="fa fa-heart" aria-hidden="true"></i>
+									<i class="fa fa-heart" style="color: red;" aria-hidden="true"></i>
 									<div class="left-info">
-										<span class="index">0 item</span>
+										@if(Cart::instance('wishlist')->count() > 0)
+										<span class="index">{{Cart::instance('wishlist')->count()}} item</span>
+										@endif
 										<span class="title">Wishlist</span>
 									</div>
 								</a>
@@ -151,8 +153,8 @@
 								<a href="#" class="link-direction">
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
-										@if(Cart::count() > 0)
-										<span class="index">{{Cart::count()}} items</span>
+										@if(Cart::instance('cart')->count() > 0)
+										<span class="index">{{Cart::instance('cart')->count()}} items</span>
 										@endif
 										<span class="title">CART</span>
 									</div>
