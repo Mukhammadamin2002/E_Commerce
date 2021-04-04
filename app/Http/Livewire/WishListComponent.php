8fc1275b68f9cb69d67d502198bd380b4bdf4b95
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-04-04 11:15:12 +0500

    Wishlist List Page Created

--- app/Http/Livewire/WishListComponent.php
+++ app/Http/Livewire/WishListComponent.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use Cart;
 use Livewire\Component;
 
 class WishListComponent extends Component
 {
+    public function removeFromWishList($product_id)
+    {
+        foreach (Cart::instance('wishlist')->content() as $witem) {
+            if ($witem->id == $product_id) {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-count-component', 'refreshComponent');
+                return;
+            }
+        }
+    }
+
     public function render()
     {
-        return view('livewire.wish-list-component');
+        return view('livewire.wishlist-component')->layout('layouts.base');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\WishListComponent;
 use Illuminate\Support\Facades\Route;
 
 
@@ -38,44 +39,44 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/',HomeComponent::class);
+Route::get('/', HomeComponent::class);
 
-Route::get('/shop',ShopComponent::class);
+Route::get('/shop', ShopComponent::class);
 
-Route::get('/cart',CartComponent::class)->name('product.cart');
+Route::get('/cart', CartComponent::class)->name('product.cart');
 
-Route::get('/checkout',CheckoutComponent::class);
+Route::get('/checkout', CheckoutComponent::class);
 
-Route::get('product/{slug}',DetailsComponent::class)->name('product.details');
+Route::get('product/{slug}', DetailsComponent::class)->name('product.details');
 
-Route::get('/product-category/{category_slug}',CategoryComponent::class)->name('product.category');
+Route::get('/product-category/{category_slug}', CategoryComponent::class)->name('product.category');
 
-Route::get('/search',SearchComponent::class)->name('product.search');
+Route::get('/search', SearchComponent::class)->name('product.search');
+
+Route::get('/wishlist', WishListComponent::class)->name('product.wishlist');
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
 // })->name('dashboard');
 
 // For User or Costumer
-Route::middleware(['auth:sanctum','verified'])->group(function(){
-	Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+	Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
 });
 
 // For Admin
-Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
-	Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
-	Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
-	Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
-	Route::get('/admin/category/edit/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
-	Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
-	Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
-	Route::get('/admin/product/edit/{product_slug}',AdminEditProductComponent::class)->name('admin.editproduct');
-	Route::get('/admin/slider',AdminHomeSliderComponent::class)->name('admin.homeslider');
-	Route::get('/admin/slider/add',AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
-	Route::get('/admin/slider/edit/{slide_id}',AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
+	Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+	Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
+	Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
+	Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
+	Route::get('/admin/products', AdminProductComponent::class)->name('admin.products');
+	Route::get('/admin/product/add', AdminAddProductComponent::class)->name('admin.addproduct');
+	Route::get('/admin/product/edit/{product_slug}', AdminEditProductComponent::class)->name('admin.editproduct');
+	Route::get('/admin/slider', AdminHomeSliderComponent::class)->name('admin.homeslider');
+	Route::get('/admin/slider/add', AdminAddHomeSliderComponent::class)->name('admin.addhomeslider');
+	Route::get('/admin/slider/edit/{slide_id}', AdminEditHomeSliderComponent::class)->name('admin.edithomeslider');
 
-	Route::get('admin/home-categories',AdminHomeCategoryComponent::class)->name('admin.homecategories');
-	Route::get('/admin/sale',AdminSaleComponent::class)->name('admin.sale');
-
+	Route::get('admin/home-categories', AdminHomeCategoryComponent::class)->name('admin.homecategories');
+	Route::get('/admin/sale', AdminSaleComponent::class)->name('admin.sale');
 });
- 
