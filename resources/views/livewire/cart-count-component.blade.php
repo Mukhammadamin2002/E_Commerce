05d23d789832d7d71a75bbdc7565989af01667a1
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-04-04 13:10:50 +0500

    Extra Functionalities Added on Counting Items on Wishlist and Cart

--- resources/views/livewire/cart-count-component.blade.php
+++ resources/views/livewire/cart-count-component.blade.php
@@ -1,9 +1,10 @@
 <div class="wrap-icon-section minicart">
 	<a href="{{ route('product.cart') }}" class="link-direction">
-		<i class="fa fa-shopping-basket" style="color: #033047;" aria-hidden="true"></i>
+		<i class="fa fa-shopping-basket" style="color: #033047" aria-hidden="true"></i>
 		<div class="left-info">
 			@if(Cart::instance('cart')->count() > 0)
-			<span class="index">{{Cart::instance('cart')->count()}} items</span>
+			<span class="index">{{Cart::instance('cart')->count()}} 
+				{{ Str::plural('item',Cart::instance('cart')->count()) }}</span>
 			@endif
 			<span class="title">CART</span>
 		</div>
--- resources/views/livewire/wishlist-count-component.blade.php
+++ resources/views/livewire/wishlist-count-component.blade.php
@@ -1,9 +1,10 @@
 <div class="wrap-icon-section wishlist">
 	<a href="{{ route('product.wishlist') }}" class="link-direction">
-		<i class="fa fa-heart" style="color: red;" aria-hidden="true"></i>
+		<i class="fa fa-heart" style="color: red" aria-hidden="true"></i>
 		<div class="left-info">
 			@if(Cart::instance('wishlist')->count() > 0)
-			<span class="index">{{Cart::instance('wishlist')->count()}} item</span>
+			<span class="index">{{Cart::instance('wishlist')->count()}} 
+				{{ Str::plural('item',Cart::instance('wishlist')->count()) }}</span>
 			@endif
 			<span class="title">Wishlist</span>
 		</div>
